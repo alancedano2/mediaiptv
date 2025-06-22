0af3c79af18a1780ea5592ac9a4e0eb06264fbff
alancedano2 <supernatura94@yahoo.com>
2025-06-21 21:47:07 -0400

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -5,13 +5,13 @@ export const config = {
 export default async function handler(req) {
   const targetUrl = new URL(req.url).searchParams.get('url');
   if (!targetUrl) {
-    return new Response('Error: No se proporcionó una URL de destino.', { status: 400 });
+    return new Response('Error: No URL provided.', { status: 400 });
   }
 
   const fetchUrl = new URL(targetUrl);
   const response = await fetch(fetchUrl.toString(), {
     headers: {
-      'User-Agent': req.headers.get('User-Agent') || 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+      'User-Agent': req.headers.get('User-Agent') || 'Mozilla/5.0',
       'Referer': fetchUrl.origin,
     },
   });
@@ -23,10 +23,9 @@ export default async function handler(req) {
     const rewrittenPlaylist = playlistContent.replace(/^(?!#)(.*)$/gm, (line) => {
       if (line.startsWith('http')) {
         return `/api/proxy?url=${encodeURIComponent(line)}`;
-      } else {
-        const absoluteUrl = new URL(line, baseUrl).toString();
-        return `/api/proxy?url=${encodeURIComponent(absoluteUrl)}`;
       }
+      const absoluteUrl = new URL(line, baseUrl).toString();
+      return `/api/proxy?url=${encodeURIComponent(absoluteUrl)}`;
     });
     return new Response(rewrittenPlaylist, { headers: { 'Content-Type': contentType, 'Access-Control-Allow-Origin': '*' }});
   }
